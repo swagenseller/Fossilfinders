1d74293656cfce1bb22a306325a3d3db7381fa02
Jeff <jwagense@ucsc.edu>
2021-03-22 11:17:23 -0700

    created board component that displays game boards

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,15 +1,40 @@
 import { useEffect, useState } from "react";
+import "../css/board.css";
+
+const cart = [
+	["Corn", "Potato", "Radish"],
+	["Tomato", "Graphes", "Mango"],
+];
 
 export default function Board(props) {
-	const [turn, setTurn] = useState();
+	const [turn, setTurn] = useState(props.turn);
 
-	useEffect(() => {
+	/*useEffect(() => {
 		setTurn(props.turn);
-	}, [props.turn]);
+	}, [props.turn]); */
+
+	const [topGrid, setTopGrid] = useState();
+	const [bottomGrid, setBottomGrid] = useState();
+	/*useEffect(() => {
+		setBoard(props.board);
+	}, [props.board]); */
+
+	const displayGrid = (grid) => {
+		return grid.map((items, index) => {
+			return (
+				<tr>
+					{items.map((subItems, sIndex) => {
+						return <td> {subItems} </td>;
+					})}
+				</tr>
+			);
+		});
+	};
 
 	return (
 		<div>
 			<h1>Turn: {turn}</h1>
+			<table>{displayGrid(cart)}</table>
 		</div>
 	);
 }
