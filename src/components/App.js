9841bc17254b070e693e45a08e50a04e755a26a7
Jeff <jwagense@ucsc.edu>
2021-03-22 14:05:07 -0700

    created a test 2d array for the board

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import Board from "./Board";
+
+const testBoard = [];
+for (let i = 0; i < 8; i++) {
+	testBoard.push([]);
+	for (let j = 0; j < 8; j++) {
+		testBoard[i].push({ visible: false, fossil: false });
+	}
+}
+
+//console.log(testBoard);
+
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<Board turn="red" />
+				<Board turn="red" board={testBoard} />
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
