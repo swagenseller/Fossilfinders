e3d6877182178f161fcd0c611316179f029eb537
Jeff <jwagense@ucsc.edu>
2021-04-03 17:25:16 -0700

    added a module that places a fossil on the board

--- src/components/randBoard.js
+++ src/components/randBoard.js
@@ -2,3 +2,18 @@
 function get_random(Min, Max) {
 	return Math.floor(Math.random() * (Max - Min + 1)) + Min;
 }
+
+//creates a fossil on the board
+const setFossil = (board, horizontal, x, y, fossilSize) => {
+	if (horizontal) {
+		for (var i = x; i < x + fossilSize; i++) {
+			board[i][y].fossil = true;
+		}
+	} else {
+		for (var i = y; i < y + fossilSize; i++) {
+			board[i][y].fossil = true;
+		}
+	}
+};
+
+export default setFossil;
