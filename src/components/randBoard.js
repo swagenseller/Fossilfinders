31f99125ddec70ff95c63c9277affd1396c673cc
Jeff <jwagense@ucsc.edu>
2021-04-03 18:04:07 -0700

    clarified the ordering in the 2d arrays

--- src/components/randBoard.js
+++ src/components/randBoard.js
@@ -4,14 +4,15 @@ function get_random(Min, Max) {
 }
 
 //creates a fossil on the board
+//coords for the board[y][x]
 const setFossil = (board, horizontal, x, y, fossilSize) => {
 	if (horizontal) {
 		for (var i = x; i < x + fossilSize; i++) {
-			board[i][y].fossil = true;
+			board[y][i].fossil = true;
 		}
 	} else {
 		for (var i = y; i < y + fossilSize; i++) {
-			board[i][y].fossil = true;
+			board[i][x].fossil = true;
 		}
 	}
 };
