c1cafff821ff74f5f4e43746d7dbf69ba82f8c05
Jeff <jwagense@ucsc.edu>
2021-04-12 10:32:18 -0700

    removed some code from create-react-app

--- src/components/App.js
+++ src/components/App.js
@@ -45,30 +45,16 @@ function App() {
 
 	return (
 		<div className="App">
-			<header className="App-header">
-				<div className="game-con">
-					<Board
-						turn={turn}
-						setTurn={setTurn}
-						enemy={oneGrid}
-						player={twoGrid}
-						setTwoGrid={setTwoGrid}
-						setOneGrid={setOneGrid}
-					/>
-				</div>
-
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<div className="game-con">
+				<Board
+					turn={turn}
+					setTurn={setTurn}
+					enemy={oneGrid}
+					player={twoGrid}
+					setTwoGrid={setTwoGrid}
+					setOneGrid={setOneGrid}
+				/>
+			</div>
 		</div>
 	);
 }
