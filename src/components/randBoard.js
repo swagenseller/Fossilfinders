b9e52f0d917b0b1eb4bf66a2ed49d92fc7608c3c
Jeff <jwagense@ucsc.edu>
2021-04-12 23:05:52 -0700

    changed the number/shape of pieces

--- src/components/randBoard.js
+++ src/components/randBoard.js
@@ -82,8 +82,8 @@ function createFossil(board, board_size, fossil_size) {
 function createFossils(board, board_size) {
 	var fossils = [
 		[1, 4],
-		[2, 3],
-		[3, 2],
+		[3, 3],
+		[2, 2],
 	]; // first element of every pair is number of fossils, second element is length of the fossil
 	for (var i = 0; i < fossils.length; i++) {
 		for (var count = 0; count < fossils[i][0]; count++) {
